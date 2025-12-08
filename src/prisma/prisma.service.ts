6cd1437a72fdaf6460486699f3b010a3ec067e47
shariyerShazan <shariyershazan1@gmail.com>
2025-12-09 00:40:33 +0600

    init commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
+  }
+  async onModuleInit() {
+    this.$connect();
+    console.log('Prisma connected');
+  }
+  async onModuleDestroy() {
+    this.$disconnect();
+    console.log('Prisma disconnected');
+  }
+}
