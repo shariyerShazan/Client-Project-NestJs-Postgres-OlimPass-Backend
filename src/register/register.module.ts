418401c85eb2612a241a8b36132bae553c616b6d
shariyerShazan <shariyershazan1@gmail.com>
2025-12-10 00:02:04 +0600

    Send memberShipId on mail with nodemailer.

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   controllers: [RegisterController],
-  providers: [RegisterService, PrismaService],
+  providers: [RegisterService, PrismaService , MailService],
 })
 export class RegisterModule {}
