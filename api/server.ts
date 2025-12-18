596ba49607d664537fe2282ddb5e9ff933d05bc9
shariyerShazan <shariyershazan1@gmail.com>
2025-12-19 00:05:47 +0600

    -

---
+++ api/server.ts
@@ -0,0 +1,12 @@
+// ./api/server.ts
+// import { createApp } from "src/main";
+
+
+// let cachedServer: any;
+
+// export default async function handler(req: any, res: any) {
+//   if (!cachedServer) {
+//     cachedServer = await createApp();
+//   }
+//   return cachedServer(req, res);
+// }
--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,66 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
+
+
+
+
+
+
+
+
+// // src/main.ts
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import express from 'express';
+
+// export async function createApp() {
+//   const server = express();
+
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(server),
+//   );
+
+//   app.enableCors({
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:5173',
+//       'http://localhost:5174',
+//       'http://localhost:5175',
+//     ],
+//     credentials: true,
+//   });
+
+//   app.use(cookieParser());
+
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       transform: true,
+//     }),
+//   );
+
+//   app.use(
+//     '/webhook',
+//     bodyParser.raw({ type: 'application/json' }),
+//   );
+
+//   app.setGlobalPrefix("api");
+//   await app.init();
+//   return server;
+// }
+
+
+
+
+
+
+
+
+
